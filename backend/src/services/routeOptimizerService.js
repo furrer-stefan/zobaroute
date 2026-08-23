be712337db21b398ac000d3eabb8b34aae9db452
Stefan Furrer <stefan-furrer@hotmail.com>
2026-08-23 20:16:36 +0200

    Add k-means clustering for route optimization

---
+++ backend/src/services/routeOptimizerService.js
@@ -0,0 +1,91 @@
+// phase 1 - clustering
+// all the coordination-points will be devided by the amount of teams, if they are spatially related and in the same areas
+// procedure: k means clustering
+
+// notes:
+// a "centroid" is a center of a cluster
+// datapoints will always belong to the cluster, that has the nearest centroid
+// in a next step, there are areas created and a new middlepoint/centroid for these areas are defined by the mean:
+// every x and y difference of datapoint and centroid is listed in a column and the mean average will be taken from it
+// the calculation of the neares centroid per datapoint will be done one more time
+// this will be repeated a few times, until it doesn't get more exact
+
+const METERS_PER_DEGREE  = 111320
+const MAX_ROUNDS = 100
+
+function calculateDistance(a, b) {
+    // calculate delta from both latitudes
+    const deltaLat = (b.latitude - a.latitude) * METERS_PER_DEGREE 
+
+    // calculate delta from bot longitudes with also regarding the latitude
+    const meanLat = (a.latitude + b.latitude) / 2
+    const deltaLon = (b.longitude - a.longitude) * METERS_PER_DEGREE  * Math.cos(meanLat * Math.PI / 180)
+
+    // calculate pythagoras
+    return Math.sqrt(deltaLat * deltaLat + deltaLon * deltaLon)
+}
+
+function calculateCentroid(stops) {
+    if(stops.length === 0){
+        return null
+    }
+    
+    // calculate median averages for all latitudes and longitudes of all stops
+    let sumLat = 0
+    let sumLon = 0
+    for (const stop of stops){
+        sumLat += stop.latitude
+        sumLon += stop.longitude
+    }
+    const meanLat = sumLat / stops.length
+    const meanLon = sumLon / stops.length
+
+    return { latitude: meanLat, longitude: meanLon }
+}
+
+function clusterStops(stops, teamCount) {
+    // this function returns an array of arrays: per team a list of stops (so all centroid clusters)
+    // definining centroids
+    const centroids = []    
+    for(let i = 0; i < teamCount; i++){
+        const index = Math.floor(i * stops.length / teamCount) // Math.floor always rounds the result down, length of stops will be divided by amount of teams and multiplied with current index (for 200 stops it is: 0, 50, 100, 150)
+        centroids.push({
+            latitude: stops[index].latitude,
+            longitude: stops[index].longitude
+        })
+    }
+    let clusters = []
+    for (let round = 0; round < MAX_ROUNDS; round++) {
+        clusters = []
+        for(let i = 0; i < teamCount; i++){
+            clusters.push([])
+        }
+        let hasChanged = false
+        for(const stop of stops){
+            let nearestDistance = Infinity
+            let nearestClusterIndex = -1
+            for(let i = 0; i < centroids.length; i++){
+                const distance = calculateDistance(stop, centroids[i])
+                if(distance < nearestDistance){
+                    nearestDistance = distance
+                    nearestClusterIndex = i
+                }
+            }
+            if(stop.clusterIndex !== nearestClusterIndex){
+                hasChanged = true
+            }
+            clusters[nearestClusterIndex].push(stop)
+            stop.clusterIndex = nearestClusterIndex
+        }
+        if(!hasChanged){
+            break
+        }
+        for (let i = 0; i < clusters.length; i++) {
+            const newCentroid = calculateCentroid(clusters[i])
+            if(newCentroid){
+                centroids[i] = newCentroid
+            }
+        }
+    }
+    return clusters
+}
