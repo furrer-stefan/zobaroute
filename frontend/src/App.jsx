b0b8b9d71781d920713e36d4ea8e22d8241a3f20
Stefan Furrer <stefan-furrer@hotmail.com>
2026-09-11 15:00:20 +0200

    Remove general back button from last page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -31,7 +31,7 @@ function App() {
       {step === 3 && <Step3ConfigurationPage setConfig={setConfig} next={next} />}
       {step === 4 && <Step4CalculationPage config={config} setRoutes={setRoutes} routes={routes} next={next} />}
       {step === 5 && <Step5ExportPage routes={routes} goTo={goTo} setOrders={setOrders} setConfig={setConfig} setRoutes={setRoutes} />}
-      {step > 1 && <button onClick={back}>Zurück</button>}
+      {step > 1 && step < 5 && <button onClick={back}>Zurück</button>}
     </div>
   )
 }
